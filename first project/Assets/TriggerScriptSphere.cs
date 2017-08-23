d71d5a092d3f6eadee349aadd6b658c65eb3b9bc
Brad <bcb1771@gmail.com>
2017-08-23 13:55:40 -0600

    Trigger lesson

---
+++ first project/Assets/TriggerScriptSphere.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScriptSphere : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter () {
+        print("Sphere- please leave");
+	}
+	
+	// Update is called once per frame
+	void OnTriggerExit () {
+        print("Sphere- Fine I didn't like you anyways");
+	}
+}
